11b5f02f8d25947dbe52e2cb636f90b1faa11712
Ivan Auda <ivan.auda@hva.nl>
2025-02-01 00:36:39 +0100

    Added ZPacketList Composable

--- app/src/main/java/com/ztechno/applogclient/ui/ZPacketList.kt
+++ app/src/main/java/com/ztechno/applogclient/ui/ZPacketList.kt
@@ -1,4 +1,27 @@
 package com.ztechno.applogclient.ui
 
-class ZPacketList {
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Modifier
+import com.ztechno.applogclient.http.ZPacket
+
+@Composable
+fun ZPacketList(list: SnapshotStateList<ZPacket>?) {
+  val mList = list ?: remember { mutableStateListOf() }
+  LazyColumn(
+    modifier = Modifier
+//      .verticalScroll(rememberScrollState())
+      .fillMaxHeight(0.9f)
+  ) {
+    items(items = mList) {
+      ZCard(it)
+    }
+  }
+  Text("...")
 }
